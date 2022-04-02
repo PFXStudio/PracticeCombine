819e063a014858866cda388e590c8849fe8cf817
pfxstudio21 <pfxstudio21@gmail.com>
2022-04-02 13:34:18 +0900

    update combine

--- PracticeCombine/PracticeCombine/ViewController.swift
+++ PracticeCombine/PracticeCombine/ViewController.swift
@@ -18,7 +18,7 @@ final class ViewController: UIViewController, Presentable {
     @IBOutlet private weak var indicatorView: UIActivityIndicatorView!
     
     private var viewModel: ViewModelable = ViewModel(service: Service())
-    private var cancelBag = [AnyCancellable]()
+    private var cancelBags = [AnyCancellable]()
     private var list: [String] = []
     
     override func viewDidLoad() {
@@ -26,6 +26,8 @@ final class ViewController: UIViewController, Presentable {
         // Do any additional setup after loading the view.
         viewModel.presenter = self
         viewModel.requestAction(with: .viewDidLoad)
+        
+        testCurrentValueSubject()
     }
 }
 
@@ -46,9 +48,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         
         let item = list[indexPath.row]
         cell.prepare(with: item)
-        cell.action.sink { _ in
-            
-        } receiveValue: { [weak self] text in
+        cell.action.sink { [weak self] _ in
             self?.viewModel.requestAction(with: .touchedInfoButton(indexPath.row))
         }
         .store(in: &cell.cancelBags)
@@ -70,8 +70,96 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     }
 }
 
+class TestSubscriptor: Subscription {
+    func request(_ demand: Subscribers.Demand) {
+        print(">>> TestSubscriptor request \(demand)")
+    }
+    
+    func cancel() {
+        print(">>> TestSubscriptor cancel")
+    }
+}
+
 /// Mark : Private functions
 private extension ViewController {
+    private func testCurrentValueSubject() {
+        let subject = CurrentValueSubject<String, Never>("ABC")
+        subject.sink {
+            print(">>> currentValueSubject \($0)")
+        }
+        .store(in: &cancelBags)
+        
+        subject.value = "DEF"
+        subject.send("GHI")
+        subject.send(completion: .finished)
+        subject.send("XXX")
+        testPassthroughSubject()
+    }
     
+    private func testPassthroughSubject() {
+        let subject = PassthroughSubject<String, Error>()
+        subject.sink {
+            print(">>> passthroughSubject error \($0)")
+        } receiveValue: {
+            print(">>> passthroughSubject \($0)")
+        }
+        .store(in: &cancelBags)
+        
+        subject.send(subscription: TestSubscriptor())
+        subject.send("Hello")
+        subject.send("World")
+        subject.send(completion: .failure(NSError(domain: "", code: -1)))
+//        subject.send(completion: .finished)
+        subject.send("XXX")
+        
+        testScheduler()
+    }
+    
+    private func testScheduler() {
+        let subject1 = CurrentValueSubject<String, Never>("KKK")
+        subject1
+            .map { _ in print(">>> subject111 \(Thread.isMainThread)") }
+            .receive(on: DispatchQueue.main)
+            .map { _ in print(">>> subject122 \(Thread.isMainThread)") }
+            .subscribe(on: DispatchQueue.global())
+            .sink {
+                print(">>> subject133 \($0) isMainThread \(Thread.isMainThread)")
+            }
+            .store(in: &cancelBags)
+
+        let subject = CurrentValueSubject<String, Never>("Thread")
+        subject
+            .subscribe(on: DispatchQueue.global())
+            .sink {
+                print(">>> subject \($0) isMainThread \(Thread.isMainThread)")
+            }
+            .store(in: &cancelBags)
+        
+        testCancellable()
+    }
+    
+    private func testCancellable() {
+        let subject = PassthroughSubject<String, Error>()
+        subject.handleEvents { subscription in
+            print(">>> testCancellable subscription11 \(subscription)")
+        } receiveOutput: { receiveOutput in
+            print(">>> testCancellable receiveOutput33 \(receiveOutput)")
+        } receiveCompletion: { receive in
+            print(">>> testCancellable receiveCompletion55 \(receive)")
+        } receiveCancel: {
+            print(">>> testCancellable receiveCancel")
+        } receiveRequest: { receiveRequest in
+            print(">>> testCancellable receiveRequest22 \(receiveRequest)")
+        }
+        .sink { type in
+            print(">>> testCancellable sink66 \(type)")
+        } receiveValue: { receiveValue in
+            print(">>> testCancellable receiveValue44 \(receiveValue)")
+        }
+        .store(in: &cancelBags)
+        subject.send("can cancel")
+        subject.send(completion: .finished)
+        subject.send(completion: .failure(NSError(domain: "cancel", code: -2)))
+    }
 }
 
